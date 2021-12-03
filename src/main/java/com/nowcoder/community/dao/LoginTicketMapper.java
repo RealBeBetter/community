7c9731c012f7a0306ab3bf231543ee8673cb3ddd
宋卫 <2411178578@qq.com>
2021-12-03 23:12:14 +0800

    添加评论 12.3

--- src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
+++ src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
@@ -11,16 +11,35 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface LoginTicketMapper {
 
-    @Insert({"insert into login_ticket(user_id, ticket, status, expired) " +
+    /*@Insert({"insert into login_ticket(user_id, ticket, status, expired) " +
             "values(#{userId}, #{ticket}, #{status}, #{expired})"})
-    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Options(useGeneratedKeys = true, keyProperty = "id")*/
+
+    /**
+     * 插入登录凭证
+     * @param loginTicket 登录凭证
+     * @return
+     */
     int insertLoginTicket(LoginTicket loginTicket);
 
-    @Select({"select id, user_id, ticket, status, expired " +
-            "from login_ticket where ticket = #{ticket}"})
+    /*@Select({"select id, user_id, ticket, status, expired " +
+            "from login_ticket where ticket = #{ticket}"})*/
+
+    /**
+     * 查询登录凭证
+     * @param ticket 登录生成的随机字符串
+     * @return LoginTicket 对象
+     */
     LoginTicket selectByTicket(String ticket);
 
-    @Update({"update login_ticket set status = #{status} " +
-            "where ticket = #{ticket}"})
+    /*@Update({"update login_ticket set status = #{status} " +
+            "where ticket = #{ticket}"})*/
+
+    /**
+     * 更新登录凭证的状态
+     * @param ticket 使用生成的字符串
+     * @param status 更改状态
+     * @return 修改的行数
+     */
     int updateStatus(String ticket, int status);
 }
